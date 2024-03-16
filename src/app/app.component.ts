fa70236c83509ac82cb8475ff453b53675867204
rajaverma <rajaverma@policybazaar.com>
2024-03-16 13:12:35 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,60 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'birthday-wish';
+  public title: string = "birthday-wish";
+  public btnTitle: string = "Let's play some music";
+  public arrangeBulbs: boolean = false;
+  public decorateTheme: boolean = false;
+  public flyBallons: boolean = false;
+  public seeCake: boolean = false;
+  public message: boolean = false;
+  public happyBday: boolean = false;
+
+  onClickBtn(event: string) {
+    var that = this;
+    if (event == "Let's play some music") {
+      let audio = new Audio();
+      audio.src = "../assets/hbd.mp3";
+      audio.loop = true;
+      audio.play();
+      setTimeout(() => {
+        that.btnTitle = "Let's arrange some bulbs";
+      }, 1000);
+    }
+    else if (event == "Let's arrange some bulbs") {
+      that.arrangeBulbs = true;
+      setTimeout(() => {
+        that.btnTitle = "Let's decorate theme";
+      }, 1000);
+    }
+    else if (event == "Let's decorate theme") {
+      that.decorateTheme = true;
+      setTimeout(() => {
+        that.btnTitle = "Let's fly some ballons";
+      }, 1000);
+    }
+    else if (event == "Let's fly some ballons") {
+      that.flyBallons = true;
+      setTimeout(() => {
+        that.btnTitle = "Let's see your cake";
+      }, 1000);
+    }
+    else if (event == "Let's see your cake") {
+      that.seeCake = true;
+      setTimeout(() => {
+        that.btnTitle = "Happy birthday @Pynk";
+      }, 1000);
+    }
+    else if (event == "Happy birthday @Pynk") {
+      that.happyBday = true;
+      setTimeout(() => {
+        that.btnTitle = "A message for you";
+      }, 1000);
+    }
+    else if (event == "A message for you") {
+      that.message = true;
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [
-    { path: "home", component: HomeComponent },
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: '**', redirectTo: 'home', pathMatch: 'full'},
-];
+export const routes: Routes = [];
